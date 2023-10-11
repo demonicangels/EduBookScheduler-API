09898ffed53f0701570f0edbb2670be9e69048c8
Hristo <i499630@fhict.nl>
2023-10-11 22:27:20 +0200

    Fixed printing elements in My Bookings

--- src/main/java/com/smartbyte/edubookschedulerbackend/EduBookSchedulerBackendApplication.java
+++ src/main/java/com/smartbyte/edubookschedulerbackend/EduBookSchedulerBackendApplication.java
@@ -2,8 +2,12 @@ package com.smartbyte.edubookschedulerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+
 public class EduBookSchedulerBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/smartbyte/edubookschedulerbackend/business/response/GetUsersBookingResponse.java
+++ src/main/java/com/smartbyte/edubookschedulerbackend/business/response/GetUsersBookingResponse.java
@@ -2,9 +2,11 @@ package com.smartbyte.edubookschedulerbackend.business.response;
 
 import com.smartbyte.edubookschedulerbackend.domain.Booking;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.*;
 
+@Getter
 @Builder
 public class GetUsersBookingResponse {
     List<Booking> bookings;
