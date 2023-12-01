9fe1d94d3adcb51eec3296225e2bf4896839949f
Ömer Faruk Koran <@I509844>
2023-12-01 13:44:21 +0100

    Availability service version 2 is done

--- src/main/java/com/smartbyte/edubookschedulerbackend/business/AvailabilityService.java
+++ src/main/java/com/smartbyte/edubookschedulerbackend/business/AvailabilityService.java
@@ -3,6 +3,7 @@ package com.smartbyte.edubookschedulerbackend.business;
 import com.smartbyte.edubookschedulerbackend.business.request.GetAvailabilityRequest;
 import com.smartbyte.edubookschedulerbackend.business.response.GetAvailabilityResponse;
 import com.smartbyte.edubookschedulerbackend.business.response.GetAvailabilityTutorResponse;
+import com.smartbyte.edubookschedulerbackend.business.response.GetTutorsNameResponse;
 import com.smartbyte.edubookschedulerbackend.business.response.GetUsersResponse;
 import com.smartbyte.edubookschedulerbackend.domain.Booking;
 import com.smartbyte.edubookschedulerbackend.domain.User;
@@ -13,5 +14,6 @@ import java.util.List;
 public interface AvailabilityService {
     List<GetAvailabilityResponse> findAvailableTeachersByDateAndTime(GetAvailabilityRequest request);
     GetUsersResponse GetTutors ();
+    GetTutorsNameResponse GetTutorsName(long id);
     GetAvailabilityTutorResponse getTutorsBooking(long id);
 }
--- src/main/java/com/smartbyte/edubookschedulerbackend/controller/AvailabilityController.java
+++ src/main/java/com/smartbyte/edubookschedulerbackend/controller/AvailabilityController.java
@@ -2,10 +2,8 @@ package com.smartbyte.edubookschedulerbackend.controller;
 
 import com.smartbyte.edubookschedulerbackend.business.AvailabilityService;
 import com.smartbyte.edubookschedulerbackend.business.request.GetAvailabilityRequest;
-import com.smartbyte.edubookschedulerbackend.business.response.GetAvailabilityResponse;
+import com.smartbyte.edubookschedulerbackend.business.response.*;
 
-import com.smartbyte.edubookschedulerbackend.business.response.GetAvailabilityTutorResponse;
-import com.smartbyte.edubookschedulerbackend.business.response.GetUsersResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +21,12 @@ public class AvailabilityController {
     @PostMapping("/getTutor")
     ResponseEntity<List<GetAvailabilityResponse>> getAvailabilityOfTeachers(@RequestBody GetAvailabilityRequest request){
         List<GetAvailabilityResponse>response = availabilityService.findAvailableTeachersByDateAndTime(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<GetTutorsNameResponse> getTutorsName(@PathVariable("id") long id){
+        GetTutorsNameResponse response = availabilityService.GetTutorsName(id);
         return ResponseEntity.ok(response);
     }
 
