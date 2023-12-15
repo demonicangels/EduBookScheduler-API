4613f3373e5a64e3436fe89788e65286b86b5979
Nikol <482269@student.fontys.nl>
2023-12-15 15:55:27 +0100

    websockets config

--- src/main/java/com/smartbyte/edubookschedulerbackend/domain/WebSocketConfig.java
+++ src/main/java/com/smartbyte/edubookschedulerbackend/domain/WebSocketConfig.java
@@ -14,11 +14,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173/").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173/");
     }
 }
